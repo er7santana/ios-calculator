b992cd2216b27e689d7d82543a99d684399a3b7b
Eliezer Sant Ana <eliezer.santana@t-systems.com>
2021-05-25 15:37:30 -0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Eliezer Sant Ana on 25/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
